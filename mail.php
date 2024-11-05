b7477515583c8c9a031a2f6a030fd4c185f3ecea
Thimash kavinda <153605024+Thimashkavinda@users.noreply.github.com>
2024-11-06 00:24:27 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,36 @@
+<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$mailheader = "From:".$name."<".$email.">\r\n";
+
+$recipient = "kavindathimash0@gmail.com";
+
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
+
+echo '
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Form</title>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Thank you for contact me. I will get back to you as soon as possible!</h1>
+        <p class="back">Go back to the <a href="index.html">homepage</a></p>
+       
+    </div>
+</body>
+</html>
+
+';
+
+?>
